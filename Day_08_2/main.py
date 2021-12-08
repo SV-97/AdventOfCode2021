a46a8c28ad356f05226296807a44319ddf857226
SV-97 <volzstefan97@googlemail.com>
2021-12-08 19:55:21 +0100

    Vectorized and optimized day 8-2

--- Day_08_2/main.py
+++ Day_08_2/main.py
@@ -4,6 +4,8 @@ import re
 from collections import Counter
 
 RE = r"(?P<signal_pattern>(?:\w+ ?)+) \| (?P<output_value>(?:\w+ ?)+)"
+# This should be an `OrderedDict`` but we're not planning on using anythhing but
+# CPython so we'll use this implementation detail for convenience.
 CODES = {
     "abcefg": 0,
     "cf": 1,
@@ -21,7 +23,7 @@ with open("./input.txt", "r") as f:
 
 signal_patterns, output_values = zip(*matches)
 
-# We want to interpret the 7-segment display as a 7-dimensional vector over Z/2Z.
+# We want to interpret the 7-segment display as a 7-dimensional vector over Z/2Z=Z₂.
 # To do this we start by mapping a-g to e_1, ..., e_7.
 ident = np.eye(7, dtype=Z2)
 ident_z2 = Z2(np.eye(7, dtype=Z2))
@@ -40,39 +42,59 @@ def to_mat(strings, allow_8=False):
         [[encode(string) for string in patterns.split(" ") if len(string) != 7 or allow_8] for patterns in strings])
 
 
+# we directly discard the 8 since it'll always be 1₇ and can't give us any information.
 encoded_patterns = to_mat(signal_patterns, False)
 encoded_patterns_z2 = Z2(encoded_patterns)
 
+# Note that the output values are the right sides of a linear system.
 right_sides = to_mat(output_values, True)
 right_sides_z2 = Z2(right_sides)
 
+# the number of ones in each vector - so the length of each code
+lengths = np.sum(encoded_patterns, axis=2)
 
-lenghts = np.sum(encoded_patterns, axis=2)
-running_sum = 0
-c = Counter()
-tens = 10 ** np.arange(3, -1, -1)  # powers of ten from 1 to 1000
-for i, mat in enumerate(encoded_patterns_z2):
-    l = [mat[lenghts[i] == lenf] for lenf in range(8)]
-    fivers = l[5].sum(axis=0)
-    sixers = l[6].sum(axis=0)
-    e_1 = l[3] - l[2]  # a
-    e_6 = (fivers + sixers) * l[2]  # f
-    e_3 = l[2] - e_6  # c
-    e_2 = (l[4] + l[2]) * sixers  # b
-    e_4 = l[4] - l[2] - e_2  # d
-    e_7 = l[5].prod(axis=0) - e_1 - e_4  # g
-    e_5 = fivers + sixers - e_4 - e_6  # e
-    transform = [e_1, e_2, e_3, e_4, e_5, e_6, e_7]
-    translate = {np.argwhere(transform[k] != 0)[
-        0][1]: a for k, a in enumerate("abcdefg")}
+# `l` allows us to look up all the vectors of a certain length
+l = [encoded_patterns_z2[lengths == k] for k in range(8)]
+l = [x.reshape(len(matches), -1, 7) if x.shape[0]
+     > len(matches) else x for x in l]
+# `fivers`` and `sixers` are sums over all the codes of length 5/6.
+# Summing like this cancels out some unknowns and allows us to
+# get some more basis vectors pretty easily.
+fivers = l[5].sum(axis=1)
+sixers = l[6].sum(axis=1)
+# We now calculate all the basis vectors
+# (could probably clean this up quite a bit with some algebra)
+# This is basically the inverse of the permutation matrix that turns the "right"
+# order into the "jumbled" one. There may be some way to get this cheaper.
+# Note that by multiplying here we're essentially doing a logical and over our
+# 7 segment display segments and by doing addition we're xor-ing.
+e_1 = l[3] - l[2]  # a
+e_6 = (fivers + sixers) * l[2]  # f
+e_3 = l[2] - e_6  # c
+e_2 = (l[4] + l[2]) * sixers  # b
+e_4 = l[4] - l[2] - e_2  # d
+e_7 = l[5].prod(axis=1) - e_1 - e_4  # g
+e_5 = fivers + sixers - e_4 - e_6  # e
+transform = np.array([e_1, e_2, e_3, e_4, e_5, e_6, e_7])
 
-    indices = [np.argwhere(lin_comb != 0) for lin_comb in right_sides_z2[i]]
-    codes = ["".join(translate[index[0]] for index in index_seq)
-             for index_seq in indices]
-    digits = [CODES["".join(sorted(code))] for code in codes]
-    running_sum += np.dot(tens, np.array(digits))
-    c.update(digits)
-print(running_sum)
+# This calculates `Z2(transform[:, k, :]) @ right_sides_z2[k].T` for all k.
+# Note that we could use `right_sides_z2` here and apply Z2 to `transform``
+# directly - but we can't do this because of current limitations in `galois`.
+# But given the operations here all play nice with our homomorphism we can
+# use the workaround that we've chosen here.
+original_encoding_z2 = Z2(transform.swapaxes(0, 1) @
+                          right_sides.swapaxes(1, 2))
+
+c = np.array([tuple(encode(k)) for k in CODES])
+cs = np.arange(10)
+
+digits = np.array([[x[0] for line in quadruple.T if (x := cs[np.all(line == c, axis=1)]).size > 0]
+                   for quadruple in original_encoding_z2])
+
+values = digits @ (10 ** np.arange(3, -1, -1))
+
+print(values.sum())
+
 """
 classes:
 length | values
