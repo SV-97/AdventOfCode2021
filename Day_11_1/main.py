8c25252b81ee8837ec2a7c4aa585040765a54dcf
SV-97 <volzstefan97@googlemail.com>
2021-12-11 12:40:11 +0100

    Added day 11

---
+++ Day_11_1/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+with open("./input.txt", "r") as f:
+    raw = f.readlines()
+grid = np.array([[int(x) for x in line.strip()]
+                for line in raw], dtype=np.uint8)
+
+
+def resolve_9s(grid, flash_locations):
+    grid = grid.copy()
+    new_flash_locations = grid > 9
+    tens = np.argwhere(new_flash_locations)
+    grid[new_flash_locations] -= 10
+    flash_locations |= new_flash_locations
+    row_idx, col_idx = np.indices(grid.shape)
+    for point in tens:
+        max_distance = np.maximum(
+            np.abs(row_idx - point[0]), np.abs(col_idx - point[1]))
+        # note that a currently flashing octopus can *not* be flashed by it's neighbors
+        grid[(max_distance == 1) & (~flash_locations)] += 1
+        grid, flash_locations = resolve_9s(grid, flash_locations)
+    return grid, flash_locations
+
+
+flashes = 0
+for t in range(100):
+    grid, flash_locations = resolve_9s(
+        grid + 1, np.zeros(grid.shape, np.bool8))
+    flashes += np.sum(flash_locations)
+    # print("\n".join("".join(line) for line in grid.astype(str)), "\n")
+
+print(flashes)
